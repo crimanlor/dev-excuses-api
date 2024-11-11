5adb0baadc97240569cc6c1c8380c89a22091c2e
Lorena Criado <criado.manzaneque@gmail.com>
2024-11-11 16:38:27 +0100

    #3 Issue: getExcuse controller has been changed to confirm all the new excuses are in DB

--- controllers/api.js
+++ controllers/api.js
@@ -2,9 +2,13 @@ const Excuse = require("../models/excuse.model");
 
 
 const getExcuse = async (req, res) => {
-    const excuses = await Excuse.find()
-    const excuse = excuses[Math.floor(Math.random() * excuses.length)];
-    res.json({ excuse });
+     try {
+        const excuses = await Excuse.find();
+        res.json({ excuses });
+    } catch (error) {
+        console.error("Error fetching excuses:", error);
+        res.status(500).json({ message: "Error fetching excuses" });
+    }
 }
 
 const postNewExcuse = async (req, res) => {
